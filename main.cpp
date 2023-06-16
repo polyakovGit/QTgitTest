98d2e90e396ee371086e44a6499b2296b8c29873
igorPolyakov <polyakovmymail@gmail.com>
2023-06-16 22:18:32 +0500

    Second fix

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
-#include "widget.h"
-
 #include <QApplication>
-
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    QWidget window;
+    window.setWindowTitle("Первая программа на Qt");
+    window.resize(300, 70);
+    QLabel *label = new QLabel("<center>Привет, мир!</center>");
+    QPushButton *btnQuit = new QPushButton("&Закрыть окно");
+    QVBoxLayout *vbox = new QVBoxLayout();
+    vbox->addWidget(label);
+    vbox->addWidget(btnQuit);
+    window.setLayout(vbox);
+    QObject::connect(btnQuit, SIGNAL(clicked()),
+                     &app, SLOT(quit()));
+    window.show();
+    return app.exec();
 }
